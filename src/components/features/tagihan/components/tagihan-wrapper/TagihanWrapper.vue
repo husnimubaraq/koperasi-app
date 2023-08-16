1ff0e6014a0af91836a6e395299fb85357b38676
laili septiandi <lailiseptiandi2@gmail.com>
2023-08-16 19:59:50 +0700

    fix. hide feature forgot && no hp

--- src/components/features/tagihan/components/tagihan-wrapper/TagihanWrapper.vue
+++ src/components/features/tagihan/components/tagihan-wrapper/TagihanWrapper.vue
@@ -1,30 +1,29 @@
 <script setup lang="ts">
-import { useAuthStore } from '~/stores/auth';
+import { useAuthStore } from "~/stores/auth";
 
-const { user } = useAuthStore()
+const { user } = useAuthStore();
 
-const isAdmin = user?.role === 'admin'
-
+const isAdmin = user?.role === "admin";
 </script>
 
 <template>
   <div class="container p-10 flex-1">
     <div class="border-b border-black pb-8 w-[80%] mb-5">
-        <p class="text-3xl capitalize mb-4 font-semibold">Tagihan</p>
-        <div v-if="!isAdmin">
-          <p class="text-2xl capitalize">{{ user?.name }}</p>
-          <p class="">Nasabah - 0812312332</p>
-          <p class="mt-2 text-gray-400">Anda memiliki tagihan sebesar Rp -</p>
+      <p class="text-3xl capitalize mb-4 font-semibold">Tagihan</p>
+      <div v-if="!isAdmin">
+        <p class="text-2xl capitalize">{{ user?.name }}</p>
+        <p class="">Nasabah</p>
+        <p class="mt-2 text-gray-400">Anda memiliki tagihan sebesar Rp -</p>
+      </div>
+      <div v-else>
+        <div class="flex">
+          <base-button variant="primary" to="/tagihan/tambah-tagihan">
+            <p class="mx-5">Tambah</p>
+          </base-button>
         </div>
-        <div v-else>
-          <div class="flex">
-            <base-button variant="primary" to="/tagihan/tambah-tagihan">
-              <p class="mx-5">Tambah</p>
-            </base-button>
-          </div>
-        </div>
+      </div>
     </div>
 
-    <tagihan-table/>
+    <tagihan-table />
   </div>
 </template>
