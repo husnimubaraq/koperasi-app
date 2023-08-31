6659a1cf73f6e64bb667c126ae10715c93408a16
sangkutikhlas <sangkutikhlas25@gmail.com>
2023-08-31 19:11:43 +0700

    revisi

--- src/components/features/bantuan/components/bantuan-panduan/BantuanPanduan.vue
+++ src/components/features/bantuan/components/bantuan-panduan/BantuanPanduan.vue
@@ -1,26 +1,36 @@
 <script setup lang="ts">
-import { VuePDF, usePDF } from '@tato30/vue-pdf'
-import { pdfFile } from '../../data'
-
-const { pdf, pages } = usePDF({
-    url: pdfFile
-})
-
-const page = ref(1)
 
 </script>
 
 <template>
-    <div class="mt-5 flex flex-col items-center bg-[#EFEDED] pb-5">
-        <div class="flex items-center gap-x-3 py-5">
-            <button @click="page = page > 1 ? page - 1 : page" class="hover:text-[#29A073]">
-                Prev
-            </button>
-            <span>{{ page }} / {{ pages }}</span>
-            <button @click="page = page < pages ? page + 1 : page" class="hover:text-[#29A073]">
-                Next
-            </button>
+    <div class="mt-5">
+        <p>PRESENTASE PINJAMAN PADA KOPERASI</p>
+        <div class="bg-[#EFEDED] p-3 rounded-md mt-3">
+          <p>Contoh Perhitungan Pinjaman</p><br/>
+            <p>Rp 10.000.000 ((Rp10.000.000 x 1%) / 30) x 12 bulan = Rp 10.000</p><br/> 
+            <p>jumlah Bayar Perbulan =RP 843.333.333</p><br/>
+            <p>Rp 10.000.000	((Rp10.000.000 x 1%) / 30) x 11 bulan = Rp 20.000</p><br/>
+            <p>jumlah Bayar Perbulan =RP 929.090.909.333</p><br/>
+            <p>Rp 10.000.000	((Rp10.000.000 x 1%) / 30) x 10 bulan = Rp 30.000</p><br/>
+            <p>jumlah Bayar Perbulan =RP 1.030.000</p><br/>
+            <p>Rp 10.000.000	((Rp10.000.000 x 1%) / 30) x 9 bulan = Rp 40.000</p><br/>
+            <p>jumlah Bayar Perbulan =RP 1.151.111</p><br/>
+            <p>Rp 10.000.000	((Rp10.000.000 x 1%) / 30) x 8 bulan = Rp 50.000</p><br/>
+            <p>jumlah Bayar Perbulan =RP 1.300.000</p><br/>
+            <p>Rp 10.000.000	((Rp10.000.000 x 1%) / 30) x 7 bulan = Rp 60.000</p><br/>
+            <p>jumlah Bayar Perbulan =RP 1.488.571</p><br/>
+            <p>Rp 10.000.000	((Rp10.000.000 x 1%) / 30) x 6 bulan = Rp 70.000</p><br/>
+            <p>jumlah Bayar Perbulan =RP 1.736.666</p><br/>
+            <p>Rp 10.000.000	((Rp10.000.000 x 1%) / 30) x 5 bulan = Rp 80.000</p><br/>
+            <p>jumlah Bayar Perbulan =RP 2.080.000</p><br/>
+            <p>Rp 10.000.000	((Rp10.000.000 x 1%) / 30) x 4 bulan = Rp 90.000</p><br/>
+            <p>jumlah Bayar Perbulan =RP 2.590.000</p><br/>
+            <p>Rp 10.000.000	((Rp10.000.000 x 1%) / 30) x 3 bulan = Rp 100.000</p><br/>
+            <p>jumlah Bayar Perbulan =RP 3.433.333</p><br/>
+            <p>Rp 10.000.000	((Rp10.000.000 x 1%) / 30) x 2 bulan = Rp 110.000</p><br/>
+            <p>jumlah Bayar Perbulan =RP 5.110.000</p><br/>
+            <p>Rp 10.000.000	((Rp10.000.000 x 1%) / 30) x 1 bulan = Rp 120.000</p><br/>
+            <p>jumlah Bayar Perbulan =RP 10.120.000</p><br/>
         </div>
-        <VuePDF :pdf="pdf" :page="page" />
     </div>
 </template>
